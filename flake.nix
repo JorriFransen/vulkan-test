41a2cc30ceff28aaf7e52e29be58882b4326d5e4
Jorri Fransen <jorrifransen@gmail.com>
2024-12-01 23:05:40 +0100

    Flake.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+# https://discourse.nixos.org/t/how-to-build-the-zig-language-server-using-zig-overlay-nix-flake/43580/3
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zls-overlay.url = "github:zigtools/zls";
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    # zig = inputs.zig-overlay.packages.x86_64-linux.default;
+    zig = inputs.zig-overlay.packages.x86_64-linux.master;
+    zls = inputs.zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
+            nativeBuildInputs = [ zig ];
+          });
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        zls
+        zig
+      ];
+
+      shellHook = ''
+        $SHELL
+      '';
+    };
+  };
+}
+
