0c52049e9a300c46c0fd63b7fd24b7c43ae28e56
Jorri Fransen <jorrifransen@gmail.com>
2024-12-11 22:56:24 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'zig-overlay':
        'github:mitchellh/zig-overlay/4e881be4521f123ad42b27f498b12d533b252b57?narHash=sha256-ow5onhv9g0zekp3TeCnKD8f1n8G7jbh4wh9XfrohrsE%3D' (2024-12-01)
      → 'github:mitchellh/zig-overlay/ed2e9c6fc49604e90a5793c3015373190748d69a?narHash=sha256-Y1dLasHOrvcic2SlrAuqDa3SaIoiNe1eOi/H3QyaUTw%3D' (2024-12-11)
    • Updated input 'zls-overlay':
        'github:zigtools/zls/532cc2598450729d064dc0630d8782af1265f999?narHash=sha256-i33Ez/uYy6VzhByudLOUlNTMmqb%2BT%2Bgu5m0nEyMr7wA%3D' (2024-12-01)
      → 'github:zigtools/zls/475463a7504c49e00cd579c24eb3e46ff9b01de0?narHash=sha256-jGnst%2B1qcMZ8%2BVZZ0PGGyV4euqxSY%2B/Y8QUay/ZM3r8%3D' (2024-12-11)
    • Updated input 'zls-overlay/flake-utils':
        'github:numtide/flake-utils/c1dfcf08411b08f6b8615f7d8971a2bfa81d5e8a?narHash=sha256-X6rJYSESBVr3hBoH0WbKE5KvhPU5bloyZ2L4K60/fPQ%3D' (2024-09-17)
      → 'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b?narHash=sha256-l0KFg5HjrsfsO/JpG%2Br7fRrqm12kzFHyUHqHCVpMMbI%3D' (2024-11-13)
    • Updated input 'zls-overlay/nixpkgs':
        'github:NixOS/nixpkgs/080166c15633801df010977d9d7474b4a6c549d7?narHash=sha256-xKel5kd1AbExymxoIfQ7pgcX6hjw9jCgbiBjiUfSVJ8%3D' (2024-10-30)
      → 'github:NixOS/nixpkgs/4dc2fc4e62dbf62b84132fe526356fbac7b03541?narHash=sha256-FillH0qdWDt/nlO6ED7h4cmN%2BG9uXwGjwmCnHs0QVYM%3D' (2024-12-05)
    • Updated input 'zls-overlay/zig-overlay':
        'github:mitchellh/zig-overlay/68091a14f9c3d803ba6ae13cab2a52623c15633d?narHash=sha256-1ndLLzFUh2uKaWjvH25lohGCSpBXANwyVmhGCDBFM6c%3D' (2024-11-03)
      → 'github:mitchellh/zig-overlay/40e32dd7e083eab4340acd17de47f13d883f257f?narHash=sha256-o4jsVKtInTA0CqwUUdcAxCYAgNLLNo30mLzz3qVc0eY%3D' (2024-12-06)

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 # https://discourse.nixos.org/t/how-to-build-the-zig-language-server-using-zig-overlay-nix-flake/43580/3
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    zig-overlay.url = "github:mitchellh/zig-overlay";
-    zls-overlay.url = "github:zigtools/zls";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
   outputs = inputs @ {
     self,
@@ -12,14 +10,6 @@
   }:
   let
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-    # zig = inputs.zig-overlay.packages.x86_64-linux.default;
-
-    # aa7d138462602e086aacf738e4b92bfa3372bebe
-    # zig = inputs.zig-overlay.packages.x86_64-linux.master-2024-12-01;
-    #
-    # zls = inputs.zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
-    #         nativeBuildInputs = [ inputs.zig-overlay.packages.x86_64-linux.master-2024-12-01 ];
-    #       });
   in
   {
     devShells.x86_64-linux.default = pkgs.mkShell {
@@ -32,6 +22,7 @@
         pkgs.glfw
 
         pkgs.xorg.libX11
+        pkgs.wayland
 
         pkgs.vulkan-tools
         pkgs.vulkan-headers
@@ -46,9 +37,7 @@
       VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
 
       shellHook = ''
-        unset WAYLAND_DISPLAY;
-        $SHELL
-        exit
+        # unset WAYLAND_DISPLAY;
       '';
     };
   };
